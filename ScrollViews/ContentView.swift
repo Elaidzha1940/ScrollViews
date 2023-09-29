ba95b2b6232ebb761dd3e171245e7a3ce2dc9434
Elaidzha1940 <eli080293@gmail.com>
2023-09-29 20:23:23 +0300

    ScrollViews + added MockData

--- ScrollViews/ContentView.swift
+++ ScrollViews/ContentView.swift
@@ -12,12 +12,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-          
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(MockData.items) { item in
+                    Rectangle()
+                        .frame(width: 90, height: 90)
+                        .cornerRadius(20)
+                        .foregroundStyle(item.color.gradient)
+                }
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
+}
+
+struct Item: Identifiable {
+    let id = UUID()
+    let color: Color
+}
+struct MockData {
+    static var items = [Item(color: .white),
+                        Item(color: .black),
+                        Item(color: .white),
+                        Item(color: .black),
+                        Item(color: .white),
+                        Item(color: .black),
+                        Item(color: .white),
+                        Item(color: .black)]
 }
