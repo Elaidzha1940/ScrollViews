01f2fa3ed21f756f9460dc038fcd8274ee0b9d66
Elaidzha1940 <eli080293@gmail.com>
2023-09-29 20:12:30 +0300

    ScrollViews

---
+++ ScrollViews/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: ScrollViews
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.09.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrollViews/ScrollViewsApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: ScrollViews
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.09.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct ScrollViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
